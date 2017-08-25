8c518936d1df41aa8d829dbcf6e35c2287459a9e
bigfang <bitair@gmail.com>
2017-08-25 11:41:32 +0800

    Adding Account functions

--- lib/hello_web/router.ex
+++ lib/hello_web/router.ex
@@ -18,10 +18,24 @@ defmodule HelloWeb.Router do
 
     get "/", PageController, :index
     resources "/users", UserController
+    resources "/sessions", SessionController, only: [:new, :create, :delete],
+                                              singleton: true
   end
 
   # Other scopes may use custom stacks.
   # scope "/api", HelloWeb do
   #   pipe_through :api
   # end
+
+  defp authenticate_user(conn, _) do
+    case get_session(conn, :user_id) do
+      nil ->
+        conn
+        |> Phoenix.Controller.put_flash(:error, "Login required")
+        |> Phoenix.Controller.redirect(to: "/")
+        |> halt()
+      user_id ->
+        assign(conn, :current_user, Hello.Accounts.get_user!(user_id))
+    end
+  end
 end

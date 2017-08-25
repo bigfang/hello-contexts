75e43d547fb67073bef2c6e40f8969f870172d1e
bigfang <bitair@gmail.com>
2017-08-25 13:18:54 +0800

    Adding CMS functions

--- lib/hello/cms/cms.ex
+++ lib/hello/cms/cms.ex
@@ -110,6 +110,15 @@ defmodule Hello.CMS do
     Page.changeset(page, %{})
   end
 
+  def inc_page_views(%Page{} = page) do
+    {1, [%Page{views: views}]} =
+      Repo.update_all(
+        from(p in Page, where: p.id == ^page.id),
+        [inc: [views: 1]], returning: [:views])
+
+    put_in(page.views, views)
+  end
+
   alias Hello.CMS.Author
 
   @doc """
